267add1231cc27e6a840037e47679003e57e4c3e
Hazakura2024 <ytr240.jp@gmail.com>
2025-12-30 22:22:36 +0900

    feat: applicationくらすを作成

--- app.py
+++ app.py
@@ -9,5 +9,21 @@ root.title('マイアプリ')
 # NOTE: 画面の横*縦のサイズを設定
 root.geometry('800x600')
 
+# NOTE: 部品を一つのまとまりとして扱うクラスを作成、それが書かれたtkinter.Frameクラスを継承
+class Application(tkinter.Frame):
+    
+    # NOTE: アプリの土台を受け取っておく
+    def __init__(self,root):
+        # NOTE: 基底クラス(Tkinter.Frame)のイニシャライザを呼び、アプリの土台を渡す
+        super().__init__(root, 
+                         width=420, 
+                         height=320, 
+                         borderwidth = 4, 
+                         relief='groove')
+        self.pack()
+        self.pack_propagate(0)
+        self.root = root
+
+
 # NOTE: アプリ起動(本来はこの前に部品を作成)
 root.mainloop()
