805cf4797ef7e5c339a36416c2b0014f2867a214
Hazakura2024 <ytr240.jp@gmail.com>
2025-12-30 22:10:59 +0900

    feat: 画面を作成

---
+++ app.py
@@ -0,0 +1,13 @@
+import tkinter
+
+# NOTE: アプリの土台
+root = tkinter.Tk()
+
+# NOTE: アプリのタイトル、画面上のバーに表示
+root.title('マイアプリ')
+
+# NOTE: 画面の横*縦のサイズを設定
+root.geometry('800x600')
+
+# NOTE: アプリ起動(本来はこの前に部品を作成)
+root.mainloop()
